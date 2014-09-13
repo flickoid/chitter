9239ca58b66d5c8d951744709b5a56697587b40d
flickoid <chris.oatley@gmail.com>
2014-09-13 23:19:05 +0100

    Added password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -10,15 +10,21 @@ feature "User signs up" do
     expect(User.first.name).to eq("Chris Oatley")
   end
 
+  scenario "with a password that doesn't match" do
+    expect{ sign_up('Anna', 'aa', 'a@gmail.com', 'bunnies', 'wrong') }.to change(User, :count).by(0)
+  end
+
   def sign_up(name = "Chris Oatley",
               username = "flickoid",
               email = "chris.oatley@gmail.com",
-              password = "s3cr3t")
+              password = "s3cr3t",
+              password_confirmation = "s3cr3t")
     visit '/users/new'
     fill_in :name, :with => name
     fill_in :username, :with => username
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 end
